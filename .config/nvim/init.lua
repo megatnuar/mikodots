df960640443e34e6191ec3f7478fb1f0172406f9
Megat Nurul Anwar <megatnuar123@gmail.com>
2025-08-15 23:32:40 +0800

    added molten (jupyter) to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+--vim.cmd("runtime! plugin/rplugin.vim")
+
+--vim.g.python3_host_prog = vim.fn.exepath("python3")
+--vim.g.loaded_python3_provider = nil
