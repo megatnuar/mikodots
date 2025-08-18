982108548b00b521a7a7aad8fc13606b10731051
Megat Nurul Anwar <megatnuar123@gmail.com>
2025-08-19 01:04:09 +0800

    added molten-nvim for notebook support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
---vim.cmd("runtime! plugin/rplugin.vim")
-
---vim.g.python3_host_prog = vim.fn.exepath("python3")
---vim.g.loaded_python3_provider = nil
--- .config/nvim/lua/plugins/molten.lua
+++ .config/nvim/lua/plugins/molten.lua
@@ -8,6 +8,62 @@ return {
       -- these are examples, not defaults. Please see the readme
       vim.g.molten_image_provider = "image.nvim"
       vim.g.molten_output_win_max_height = 20
+      -- I find auto open annoying, keep in mind setting this option will require setting
+      -- a keybind for `:noautocmd MoltenEnterOutput` to open the output again
+      --vim.g.molten_auto_open_output = true
+      -- optional, I like wrapping. works for virt text and the output window
+      vim.g.molten_wrap_output = true
+      -- Output as virtual text. Allows outputs to always be shown, works with images, but can
+      -- be buggy with longer images
+      vim.g.molten_virt_text_output = true
+
+      -- this will make it so the output shows up below the \`\`\` cell delimiter
+      vim.g.molten_virt_lines_off_by_1 = true
+
+      vim.keymap.set(
+        "n",
+        "<localleader>me",
+        ":MoltenEvaluateOperator<CR>",
+        { desc = "evaluate operator", silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<localleader>os",
+        ":noautocmd MoltenEnterOutput<CR>",
+        { desc = "open output window", silent = true }
+      )
+      vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>", { desc = "re-eval cell", silent = true })
+      vim.keymap.set(
+        "v",
+        "<localleader>r",
+        ":<C-u>MoltenEvaluateVisual<CR>gv",
+        { desc = "execute visual selection", silent = true }
+      )
+      vim.keymap.set("n", "<localleader>oh", ":MoltenHideOutput<CR>", { desc = "close output window", silent = true })
+      vim.keymap.set("n", "<localleader>md", ":MoltenDelete<CR>", { desc = "delete Molten cell", silent = true })
+
+      -- if you work with html outputs:
+      vim.keymap.set(
+        "n",
+        "<localleader>mx",
+        ":MoltenOpenInBrowser<CR>",
+        { desc = "open output in browser", silent = true }
+      )
+
+      local runner = require("quarto.runner")
+      vim.keymap.set("n", "<localleader>rc", runner.run_cell, { desc = "run cell", silent = true })
+      vim.keymap.set("n", "<localleader>ra", runner.run_above, { desc = "run cell and above", silent = true })
+      vim.keymap.set("n", "<localleader>rA", runner.run_all, { desc = "run all cells", silent = true })
+      vim.keymap.set("n", "<localleader>rl", runner.run_line, { desc = "run line", silent = true })
+      vim.keymap.set("v", "<localleader>r", runner.run_range, { desc = "run visual range", silent = true })
+      vim.keymap.set("n", "<localleader>RA", function()
+        runner.run_all(true)
+      end, { desc = "run all cells of all languages", silent = true })
+
+      -- status line
+      --require("molten.status").initialized() -- "Molten" or "" based on initialization information
+      --require("molten.status").kernels() -- "kernel1 kernel2" list of kernels attached to buffer or ""
+      --require("molten.status").all_kernels() -- same as kernels, but will show all kernels
     end,
   },
   {
