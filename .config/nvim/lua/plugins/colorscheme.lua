85730fd8f316dd5541034fb730e63388de937c3a
Megat Nurul Anwar <megatnuar123@gmail.com>
2025-08-19 02:24:19 +0800

    added tokyonight-night colorscheme

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  -- add tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night" },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
