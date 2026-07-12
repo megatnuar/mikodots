97d2a477fc556fc052ae55773da82122b54bbcb7
Megat Nurul Anwar <megatnuar123@gmail.com>
2026-07-12 18:50:06 +0800

    stuff

--- .config/hypr/modules/autostart.lua
+++ .config/hypr/modules/autostart.lua
@@ -9,7 +9,8 @@
 --
 hl.on("hyprland.start", function()
 	hl.exec_cmd("hypridle")
-	hl.exec_cmd("waybar")
+	--	hl.exec_cmd("waybar")
+	hl.exec_cmd("quickshell --path ~/.config/quickshell")
 	hl.exec_cmd("swaync")
 	hl.exec_cmd("waypaper --restore")
 	hl.exec_cmd("hyprsunset --temperature 4500")
